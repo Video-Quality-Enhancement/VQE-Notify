3b08080ffd1a23d5adb7529c1e3e54392ee6295e
Deven-14 <deven.paramaj@gmail.com>
2023-05-24 04:07:27 +0530

    converted the string discord_user_id to int

--- repositories/repository.py
+++ repositories/repository.py
@@ -5,6 +5,6 @@ class Repository:
     def __init__(self, db):
         self.db = db
     
-    def get_discord_user_id(self, userId: str):
+    def get_discord_user_id(self, userId: str) -> int:
         # get discord user id from firebase db
-        return os.getenv("TEMP_USER_ID")
+        return int(os.getenv("TEMP_USER_ID")) # ! int conversion is very important here
