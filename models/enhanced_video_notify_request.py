f202a827fb0c35a26ea60929820d99cba4ef3b33
Deven-14 <deven.paramaj@gmail.com>
2023-05-24 04:06:38 +0530

    added responseInterfaces coz of unpacking error

--- models/enhanced_video_notify_request.py
+++ models/enhanced_video_notify_request.py
@@ -2,15 +2,17 @@ import json
 
 class EnhancedVideoNotifyRequest:
 
-    def __init__(self, userId: str, requestId: str, enhancedVideoUrl: str, status: str):
+    def __init__(self, userId: str, requestId: str, enhancedVideoUrl: str, status: str, responseInterfaces: list):
         self.userId = userId
         self.requestId = requestId
         self.enhancedVideoUrl = enhancedVideoUrl
+        self.responseInterfaces = responseInterfaces
         self.status = status
 
     def __str__(self):
         return f"EnhancedVideoNotifyRequest({self.__dict__})"
 
+    @classmethod
     def loads(cls, bytes):
         d = json.loads(bytes)
         return cls(**d)
