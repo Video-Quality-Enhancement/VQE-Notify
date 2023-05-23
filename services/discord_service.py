7d660582337d6304e76db141e0748d595dd32693
Deven-14 <deven.paramaj@gmail.com>
2023-05-24 04:08:40 +0530

    fixed print statments

--- services/discord_service.py
+++ services/discord_service.py
@@ -13,11 +13,11 @@ class DiscordService:
         user = self.client.get_user(discord_user_id)
         if user is not None:
             await user.send(notify_request.enhancedVideoUrl)
-            print(notify_request) # TODO: log stuff here
+            print("discord message sent to user", user) # TODO: log stuff here
         else:
             print("user not found", discord_user_id) 
         
     def send_discord_notification(self, notify_request: EnhancedVideoNotifyRequest):
         discord_user_id = self.repository.get_discord_user_id(notify_request.userId)
         asyncio.run_coroutine_threadsafe(self.send_user_message(discord_user_id, notify_request), self.client.loop)
-        print("sent to discord") # TODO: log stuff here
+        # print("sent to discord") # TODO: log stuff here
